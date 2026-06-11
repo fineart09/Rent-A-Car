c5d36bb2a25ff00d48442042f796428697d8506a
chanashon.k <chanashon.k@entronica.in.th>
2026-06-11 13:10:25 +0700

    fixed hide menu

--- src/lib/rbac/menus.ts
+++ src/lib/rbac/menus.ts
@@ -5,12 +5,12 @@ export type MenuItem = {
 };
 
 export const menuItems: MenuItem[] = [
-  { title: "Dashboard", href: "/dashboard", roles: ["admin", "manager", "agent", "user"] },
+  { title: "Dashboard", href: "/dashboard", roles: ["admin", "manager"] },
   { title: "ระบบจัดการรถยนต์", href: "/cars", roles: ["admin", "manager", "agent"] },
   { title: "ข้อมูลลูกค้า", href: "/driver", roles: ["admin", "manager", "agent", "user"] },
   { title: "ข้อมูลบริการ/โปรโมชั่น", href: "/products", roles: ["admin", "manager", "agent", "user"] },
   { title: "บันทึกรายการเช่ารถ", href: "/booking", roles: ["admin", "manager", "agent", "user"] },
-  { title: "การชำระเงิน", href: "/payments", roles: ["admin", "manager", "agent", "user"] },
+  // { title: "การชำระเงิน", href: "/payments", roles: ["admin", "manager", "agent", "user"] },
   { title: "ตั้งค่าระบบ", href: "/setting/users", roles: ["admin", "manager"] },
 ];
 
